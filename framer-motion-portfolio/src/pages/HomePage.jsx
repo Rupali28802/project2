8407899209eabb08d28e312b404ea7553a7fd258
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-06-18 16:24:10 +0530

    homepage

--- framer-motion-portfolio/src/pages/HomePage.jsx
+++ framer-motion-portfolio/src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage= () => {
   return (
     <div>
       <section id='home'>
-        <Hero />
+        <Hero/>
       </section>
       <section id='projects'>
         <SelectedWork />
